19ac08f00d9d5830910a6decf5d0ac6e9c8396d1
Yuyakun0316 <3.16.yuyakun@gmail.com>
2025-11-08 14:54:06 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'furima46173' && password == '46173'
+    end
+  end
 end
