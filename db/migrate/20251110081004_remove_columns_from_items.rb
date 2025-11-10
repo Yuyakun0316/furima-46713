28c39aaaab2a6582c855919c13c01336978c8b86
Yuyakun0316 <3.16.yuyakun@gmail.com>
2025-11-10 18:27:52 +0900

    出品した商品の情報をテーブルに保存

--- db/migrate/20251110081004_remove_columns_from_items.rb
+++
@@ -1,9 +0,0 @@
-class RemoveColumnsFromItems < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :items, :category_id, :integer
-    remove_column :items, :condition_id, :integer
-    remove_column :items, :shipping_fee_payer_id, :integer
-    remove_column :items, :prefecture_id, :integer
-    remove_column :items, :shipping_day_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_10_081004) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_10_091037) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -56,6 +56,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_10_081004) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
+    t.integer "condition_id"
+    t.integer "shipping_fee_payer_id"
+    t.integer "prefecture_id"
+    t.integer "shipping_day_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
