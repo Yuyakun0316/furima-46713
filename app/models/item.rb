ba121aab6943d0af967b6e7ef14146e5a0f48b91
Yuyakun0316 <3.16.yuyakun@gmail.com>
2025-11-10 21:42:42 +0900

    Rubocopを実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,7 +17,8 @@ class Item < ApplicationRecord
   validates :shipping_fee_payer_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   belongs_to :user
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -93,4 +93,3 @@ RSpec.describe Item, type: :model do
     end
   end
 end
-
