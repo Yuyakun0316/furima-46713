fc9b283d1353dbd7c881c28ec869803acba09c07
Yuyakun0316 <3.16.yuyakun@gmail.com>
2025-11-11 10:36:12 +0900

    不必要なファイルの削除

--- db/migrate/20251110080139_create_conditions.rb
+++
@@ -1,8 +0,0 @@
-class CreateConditions < ActiveRecord::Migration[7.1]
-  def change
-    create_table :conditions do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20251110091037_add_columns_to_items.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnsToItems < ActiveRecord::Migration[7.1]
-  def change
-    add_column :items, :category_id, :integer
-    add_column :items, :condition_id, :integer
-    add_column :items, :shipping_fee_payer_id, :integer
-    add_column :items, :prefecture_id, :integer
-    add_column :items, :shipping_day_id, :integer
-  end
-end
