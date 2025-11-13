d7d441a4cd72c6f356a75165aecf79775387262a
Yuyakun0316 <3.16.yuyakun@gmail.com>
2025-11-13 20:50:22 +0900

    売却済みor出品者本人の場合は購入ページを表示しない。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
+  before_action :redirect_if_sold_out_or_owner, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
@@ -25,5 +26,9 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(
       user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def redirect_if_sold_out_or_owner
+    redirect_to root_path if @item.order.present? || current_user.id == @item.user_id
   end
 end
